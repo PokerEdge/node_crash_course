f2588c6a47b625fbdfe37ba8bd71595db87b4f84
Reid Young <reid@pokersprout.com>
2019-02-07 15:52:41 -0500

    Check if dir exists synchronously before mkdir
    Allow the checking of existence of the directory to complete synchronously prior to creating a directory with a particular name to avoid the 'directory already exists' error.

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-// Create folder
-// fs.mkdir(path.join(__dirname, '/test'), {}, err => {
-//   if (err) throw err;
-//   console.log('Folder created...');
-// });
+// Create folder after checking if the folder already exists
+// if(!fs.existsSync(path.join(__dirname, '/test')) {
+//   fs.mkdir(path.join(__dirname, '/test'), {}, err => {
+//     if (err) throw err;
+//     console.log('Folder created...');
+//   });
+// }
 
 // Create and write to file
 // fs.writeFile(
